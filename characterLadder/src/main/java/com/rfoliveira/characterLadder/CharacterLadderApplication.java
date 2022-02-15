9007966d52c8ac37a116d34566d6411f3b38cd84
rodrigokaconde <rodrigokaconde@yahoo.com.br>
2022-02-15 18:45:17 -0300

    create project question 01

---
+++ characterLadder/src/main/java/com/rfoliveira/characterLadder/CharacterLadderApplication.java
@@ -0,0 +1,13 @@
+package com.rfoliveira.characterLadder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterLadderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterLadderApplication.class, args);
+	}
+
+}
---
+++ characterLadder/src/test/java/com/rfoliveira/characterLadder/CharacterLadderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfoliveira.characterLadder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterLadderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

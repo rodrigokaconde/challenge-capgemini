4a02dfb0ae50ef741105700933bc36854a204885
rodrigokaconde <rodrigokaconde@yahoo.com.br>
2022-02-15 21:51:25 -0300

    create project secure string

---
+++ secureString/src/main/java/com/rfoliveira/secureString/SecureStringApplication.java
@@ -0,0 +1,13 @@
+package com.rfoliveira.secureString;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureStringApplication.class, args);
+	}
+
+}
---
+++ secureString/src/test/java/com/rfoliveira/secureString/SecureStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfoliveira.secureString;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

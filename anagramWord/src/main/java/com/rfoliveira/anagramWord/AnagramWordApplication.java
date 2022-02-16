c285063aa31aa5fcea07a3c6da1d1a92091402c5
rodrigokaconde <rodrigokaconde@yahoo.com.br>
2022-02-15 22:14:53 -0300

    create project anagram word

---
+++ anagramWord/src/main/java/com/rfoliveira/anagramWord/AnagramWordApplication.java
@@ -0,0 +1,13 @@
+package com.rfoliveira.anagramWord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnagramWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnagramWordApplication.class, args);
+	}
+
+}
---
+++ anagramWord/src/test/java/com/rfoliveira/anagramWord/AnagramWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfoliveira.anagramWord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnagramWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
